246689c67eed6619f4c0229144a886a987b09017
Gonçalo Sousa <gsousa@atlasazul.pt>
2020-02-11 16:24:15 +0000

    file structure

---
+++ autoload.php
@@ -0,0 +1,18 @@
+<?php
+define('PROJECT_PATH', __DIR__.'/');
+
+function autoload($className){
+	$className = ltrim($className, '/');
+	$fileName = '';
+	$namespace = '';
+	if($lastNsPos = strripos($className, '\\')){
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos +1);
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;		
+	}
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+	require PROJECT_PATH.$fileName;
+}
+
+spl_autoload_register('autoload');
+?>
